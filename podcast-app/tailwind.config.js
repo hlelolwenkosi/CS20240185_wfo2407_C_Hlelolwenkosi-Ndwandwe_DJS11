4c1bdff0bb0af42c0aec62899ddb270e5b55b868
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-11-27 06:46:57 +0200

    "Updated tailwind.config.js and vite.config.js files"

--- podcast-app/tailwind.config.js
+++ podcast-app/tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
